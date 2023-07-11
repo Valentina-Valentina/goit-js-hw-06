e40075c9969114ddb98e1be71a08e4f0d62aab9c
Valentina <valentinasergssvna@gmail.com>
2023-07-11 17:38:28 +0300

    task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.querySelector('#ingredients')
+const items = [];
+
+ingredients.forEach(ingredient => {
+  const li = document.createElement('li');
+  // console.log(li);
+  li.textContent = ingredient;
+  li.classList.add('item')
+  items.push(li);
+})
+  ul.append(...items);
+// console.log(ul);
