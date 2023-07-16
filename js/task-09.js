a0f297a3318f00628ade566004df983fc7d8c7e8
Valentina <valentinasergssvna@gmail.com>
2023-07-16 18:01:17 +0300

    task - 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const button = document.querySelector('button.change-color');
+const spanColor = document.querySelector('span.color');
+const body = document.querySelector('body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+button.addEventListener('click', (event) => {
+  const color = getRandomHexColor();
+  spanColor.textContent = color;
+  body.style.backgroundColor = color;
+});
+ 
